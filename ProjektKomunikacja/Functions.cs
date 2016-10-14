85e5c0204f980a77e5aa7c33d6e43c5ed9f5c586
FrodoBagins <martinez199509@gmail.com>
2016-10-14 18:17:43 +0200

    wadomos

--- ProjektKomunikacja/Functions.cs
+++ ProjektKomunikacja/Functions.cs
@@ -20,13 +20,95 @@ namespace ProjektKomunikacja
                 Console.WriteLine(baza);
             }
             Console.ReadLine();
+        }
+
+
+
+   /*     public static void cleaner()
+        {
+            for(int i =2;i<=117;i++)
+            {
+
+
+                for(int j=2;i<=1117)
+
+
+
+            }
+
+    */
+            
+
+
+
+
 
         }
 
 
 
 
+        public static void createInterface()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetWindowSize(120, 30);
+            Console.SetBufferSize(120, 30);
+
+            Console.SetCursorPosition(1, 0);
+            Console.Write("╔");
 
 
+            Console.SetCursorPosition(1, 29);
+            Console.Write("╚");
+
+            Console.SetCursorPosition(118, 0);
+            Console.Write("╗");
+
+            Console.SetCursorPosition(118, 29);
+            Console.Write("╝");
+
+            for (int i = 1; i < 29; ++i)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write("║");
+                Console.SetCursorPosition(118, i);
+                Console.Write("║");
+
+            }
+            for (int i = 2; i < 118; ++i)
+            {
+                Console.SetCursorPosition(i, 29);
+                Console.Write("═");
+                Console.SetCursorPosition(i, 0);
+                Console.Write("═");
+                Console.SetCursorPosition(i, 2);
+                Console.Write("═");
+            }
+
+
+            for (int i = 3; i < 100; i+=16)
+            {
+                
+
+                Console.SetCursorPosition(i, 0);
+                Console.Write("╔");
+
+                Console.SetCursorPosition(i+13, 0);
+                Console.Write("╗");
+
+                Console.SetCursorPosition(i, 1);
+                Console.Write("║");
+
+                Console.SetCursorPosition(i+13, 1);
+                Console.Write("║");
+
+                Console.SetCursorPosition(i, 2);
+                Console.Write("╩");
+
+                Console.SetCursorPosition(i+13, 2);
+                Console.Write("╩");
+            }
+        }
     }
 }
--- ProjektKomunikacja/Program.cs
+++ ProjektKomunikacja/Program.cs
@@ -16,43 +16,7 @@ namespace ProjektKomunikacja
 
             //  Console.WindowHeight();
 
-           Console.SetWindowSize(120,30);
-           Console.SetBufferSize(120, 30);
-
-
-           // for(int i = 0; i <10;i++)
-
-            Console.Beep(800,1000);
-
-            Console.SetCursorPosition(1, 0);
-            Console.Write("╔");
-
-            Console.SetCursorPosition(1, 29);
-            Console.Write("╚");
-
-            Console.SetCursorPosition(118, 0);
-            Console.Write("╗");
-
-            Console.SetCursorPosition(118, 29);
-            Console.Write("╝");
-
-            for (int i = 1; i < 29; ++i)
-            {
-                Console.SetCursorPosition(1, i);
-                Console.Write("║");
-                Console.SetCursorPosition(118, i);
-                Console.Write("║");
-
-            }
-
-            for (int i = 2; i < 118; ++i)
-            {
-                Console.SetCursorPosition(i, 29);
-                Console.Write("═");
-                Console.SetCursorPosition(i, 0);
-                Console.Write("═");
-
-            }
+            Functions.createInterface();
 
 
 
@@ -137,7 +101,7 @@ namespace ProjektKomunikacja
 
                 keypress = Console.ReadKey();
 
-            //    Console.Clear();
+         
 
             }
 
